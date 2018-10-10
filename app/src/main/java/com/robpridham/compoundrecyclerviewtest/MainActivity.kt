cc0034b9a521a08c0c26a3986143f35c542d95d1
Rob Pridham <rob.pridham@bbc.co.uk>
2018-10-10 09:24:44 +0100

    Basic list adapter demo

---
+++ app/src/main/java/com/robpridham/compoundrecyclerviewtest/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.robpridham.compoundrecyclerviewtest
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val adapter = BasicListAdapter()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
+        val layoutMgr = LinearLayoutManager(this)
+        layoutMgr.isAutoMeasureEnabled = false
+        recyclerView.layoutManager = layoutMgr
+
+        recyclerView.adapter = adapter
+    }
+}
